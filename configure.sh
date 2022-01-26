fd3fc2a51e73f53b2cfaaa7aff49002c3d6ef3c6
Tad <tad@live.in>
2022-01-26 14:33:33 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -18,7 +18,7 @@ cat << EOF > /usr/local/etc/xray/config.json
   },
   "inbounds": [
     {
-      "port": "8080",
+      "port": "$PORT",
       "protocol": "VLESS",
       "settings": {
         "clients": [
